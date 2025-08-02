a2ce7244437ac2ddb4ab2d7e0facfd6281e7a1b6
lhostfree951 <122357287+lhostfree951@users.noreply.github.com>
2025-08-02 10:39:07 -0300

    Add files via upload

---
+++ ActivateWindows.ps1
@@ -0,0 +1,69 @@
+$editions = @(
+    @{ Edition = "Education"; Key = "YNMGQ-8RYV3-4PGQ3-C8XTP-7CFBY"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Education.xml" },
+    @{ Edition = "Education N"; Key = "84NGF-MHBT6-FXBX8-QWJK7-DRR8H"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Education.N.xml" },
+    @{ Edition = "Enterprise"; Key = "XGVPP-NMH47-7TTHJ-W3FW7-8HV2C"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Enterprise.xml" },
+    @{ Edition = "Enterprise N"; Key = "3V6Q6-NQXCX-V8YXR-9QCYV-QPFCT"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Enterprise.N.xml" },
+    @{ Edition = "Enterprise LTSB 2015"; Key = "FWN7H-PF93Q-4GGP8-M8RF3-MDWWW"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Enterprise.LTSB.2015.xml" },
+    @{ Edition = "Enterprise LTSB 2016"; Key = "NK96Y-D9CD8-W44CQ-R8YTK-DYJWX"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Enterprise.LTSB.2016.xml" },
+    @{ Edition = "Enterprise LTSC 2019"; Key = "43TBQ-NH92J-XKTM7-KT3KK-P39PB"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Enterprise.LTSC.2019.xml" },
+    @{ Edition = "Enterprise N LTSB 2015"; Key = "NTX6B-BRYC2-K6786-F6MVQ-M7V2X"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Enterprise.N.LTSB.2015.xml" },
+    @{ Edition = "Enterprise N LTSB 2016"; Key = "2DBW3-N2PJG-MVHW3-G7TDK-9HKR4"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Enterprise.N.LTSB.2016.xml" },
+    @{ Edition = "Home"; Key = "YTMG3-N6DKC-DKB77-7M9GH-8HVX7"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Home.xml" },
+    @{ Edition = "Home N"; Key = "4CPRK-NM3K3-X6XXQ-RXX86-WXCHW"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Home.N.xml" },
+    @{ Edition = "Home China"; Key = "N2434-X9D7W-8PF6X-8DV9T-8TYMD"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Home.China.xml" },
+    @{ Edition = "Home Single Language"; Key = "BT79Q-G7N6G-PGBYW-4YWX6-6F4BT"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Home.Single.Language.xml" },
+    @{ Edition = "IoT Enterprise"; Key = "XQQYW-NFFMW-XJPBH-K8732-CKFFD"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/IoT.Enterprise.xml" },
+    @{ Edition = "IoT Enterprise Subscription"; Key = "P8Q7T-WNK7X-PMFXY-VXHBG-RRK69"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/IoT.Enterprise.Subscription.xml" },
+    @{ Edition = "IoT Enterprise LTSC 2021"; Key = "QPM6N-7J2WJ-P88HH-P3YRH-YY74H"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/IoT.Enterprise.LTSC.2021.xml" },
+    @{ Edition = "IoT Enterprise LTSC 2024"; Key = "CGK42-GYN6Y-VD22B-BX98W-J8JXD"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/IoT.Enterprise.LTSC.2024.xml" },
+    @{ Edition = "IoT Enterprise LTSC Subscription 2024"; Key = "N979K-XWD77-YW3GB-HBGH6-D32MH"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/IoT.Enterprise.LTSC.Subscription.2024.xml" },
+    @{ Edition = "Pro"; Key = "VK7JG-NPHTM-C97JM-9MPGT-3V66T"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Pro.xml" },
+    @{ Edition = "Pro N"; Key = "2B87N-8KFHP-DKV6R-Y2C8J-PKCKT"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Pro.N.xml" },
+    @{ Edition = "Pro Education"; Key = "8PTT6-RNW4C-6V7J2-C2D3X-MHBPB"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Pro.Education.xml" },
+    @{ Edition = "Pro Education N"; Key = "GJTYN-HDMQY-FRR76-HVGC7-QPF8P"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Pro.Education.N.xml" },
+    @{ Edition = "Pro for Workstations"; Key = "DXG7C-N36C4-C4HTG-X4T3X-2YV77"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Pro.for.Workstations.xml" },
+    @{ Edition = "Pro N for Workstations"; Key = "WYPNQ-8C467-V2W6J-TX4WX-WT2RQ"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Pro.N.for.Workstations.xml" },
+    @{ Edition = "S"; Key = "V3WVW-N2PV2-CGWC3-34QGF-VMJ2C"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Cloud.S.xml" },
+    @{ Edition = "S N"; Key = "NH9J3-68WK7-6FB93-4K3DF-DJ4F6"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Cloud.S.N.xml" },
+    @{ Edition = "SE"; Key = "KY7PN-VR6RX-83W6Y-6DDYQ-T6R4W"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/CloudEdition.SE.xml" },
+    @{ Edition = "SE N"; Key = "K9VKN-3BGWV-Y624W-MCRMQ-BHDCD"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/CloudEdition.SE.N.xml" },
+    @{ Edition = "Team"; Key = "XKCNC-J26Q9-KFHD2-FKTHY-KD72Y"; TicketUrl = "https://github.com/lhostfree951/hwid-kms38-tickets/releases/latest/download/Team.xml" }
+)
+
+Write-Host "Available Windows Editions:"
+for ($i = 0; $i -lt $editions.Length; $i++) {
+    Write-Host "$($i + 1). $($editions[$i].Edition)"
+}
+
+do {
+    $selection = Read-Host "Enter the number of the edition you want to use (1-$($editions.Length))"
+    $selection = [int]$selection
+} while ($selection -lt 1 -or $selection -gt $editions.Length)
+
+$selectedEdition = $editions[$selection - 1]
+$ticketUrl = $selectedEdition.TicketUrl
+$key = $selectedEdition.Key
+$ticketFileName = $ticketUrl.Split('/')[-1]
+$destinationPath = "C:\ProgramData\Microsoft\Windows\ClipSVC\GenuineTicket\$ticketFileName"
+
+$destinationDir = "C:\ProgramData\Microsoft\Windows\ClipSVC\GenuineTicket"
+if (-not (Test-Path $destinationDir)) {
+    New-Item -ItemType Directory -Path $destinationDir -Force
+}
+
+Write-Host "Downloading ticket file for $($selectedEdition.Edition)..."
+Invoke-WebRequest -Uri $ticketUrl -OutFile $destinationPath
+
+if (Test-Path $destinationPath) {
+    Write-Host "Ticket file successfully copied to $destinationPath"
+} else {
+    Write-Host "Error: Failed to download or copy the ticket file."
+    exit
+}
+
+Write-Host "`nLicense Key for $($selectedEdition.Edition): $key"
+Write-Host "`nPlease use this key in the Windows activation window."
+Write-Host "`nPress any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+Start-Process "ms-settings:activation"
